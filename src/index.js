af78e7a69ba86c69677680b8f2a5fc24b50908c4
Sarvesh Kumar <kumarsarvesh426@gmail.com>
2023-04-06 18:21:25 +0530

    Inital Commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Temp from './temp';
+
+ReactDOM.render(
+  <>
+  
+  <Temp />
+  
+  </>,
+ document.getElementById("root"));
+  
+
+
+
+
